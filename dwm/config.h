367a3c8dc816404462dacdacc11531e86a5bd5ad
jsMRSoL <si_pitt@yahoo.com>
2018-04-01 21:45:06 +0100

    2nd lap latest

--- dwm/config.h
+++ dwm/config.h
@@ -1,20 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char dmenufont[]       = "monospace:size=10";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
-static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -25,8 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Iceweasel",  NULL,       NULL,       2,       0,           -1 },
-	{ "Firefox",	NULL,	NULL,	2,	0,	-1},
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -50,16 +52,16 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/bash", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-l", "20", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "x-terminal-emulator", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
-static const char *musiccmd[] = { "urxvt", "-e", "ncmpcpp", NULL };
+static const char *musiccmd[] = { "x-terminal-emulator", "-e", "ncmpcpp", NULL };
 static const char *shutcmd[] = { "sudo", "shutdown", "-h", "now", NULL };
-static const char *filebrowse[] = { "urxvt", "-e", "ranger", NULL };
+static const char *filebrowse[] = { "x-terminal-emulator", "-e", "ranger", NULL };
 static const char *mutecmd[] = { "amixer", "-q", "sset", "Master", "toggle", NULL };
 static const char *volupcmd[] = { "amixer", "-q", "sset", "Master", "5%+", "unmute", NULL };
 static const char *voldowncmd[] = { "amixer", "-q", "sset", "Master", "5%-", "unmute", NULL };
@@ -127,3 +129,4 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
--- various/configs-sync.sh
+++ various/configs-sync.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 MYHOME='/home/simon'
-GITVAR='/home/simon/.dotfiles/various'
+GITVAR='/home/simon/.dotfiles'
 
-cp -o $MYHOME/.inputrc $GITVAR/home/inputrc
-cp -o $MYHOME/.bash_aliases $GITVAR/home/bash_aliases
-cp -o $MYHOME/.bashrc $GITVAR/home/bashrc
-cp -o $MYHOME/.selected_editor $GITVAR/home/selected_editor
-cp -o $MYHOME/.Xdefaults $GITVAR/home/Xdefaults
-cp -o $MYHOME/.profile $GITVAR/home/profile
-cp -o $MYHOME/.vimrc $GITVAR/home/vimrc
-cp -o $MYHOME/.inputrc $GITVAR/home/inputrc
+# config files under home
+cp -p $MYHOME/.inputrc $GITVAR/home/inputrc
+cp -p $MYHOME/.bash_aliases $GITVAR/home/bash_aliases
+cp -p $MYHOME/.bashrc $GITVAR/home/bashrc
+cp -p $MYHOME/.selected_editor $GITVAR/home/selected_editor
+cp -p $MYHOME/.Xdefaults $GITVAR/home/Xdefaults
+cp -p $MYHOME/.profile $GITVAR/home/profile
+cp -p $MYHOME/.vimrc $GITVAR/home/vimrc
+cp -p $MYHOME/.inputrc $GITVAR/home/inputrc
 
-cp -o /usr/share/xsessions/dwm.desktop /home/simon/.dotfiles/various/dwm.desktop
-cp -o /usr/local/bin/dwmstart $GITVAR/dwmstart
+# configs to start window manager
+cp -p /usr/share/xsessions/dwm.desktop /home/simon/.dotfiles/various/dwm.desktop
+cp -p /usr/local/bin/dwmstart $GITVAR/dwmstart
 
+# customized source files
+cp -p $MYHOME/.dwmsrc/config.h $GITVAR/dwm/
+cp -p $MYHOME/.dwmsrc/config.mk $GITVAR/dwm/
+cp -p $MYHOME/.stsrc/config.h $GITVAR/st/
+cp -p $MYHOME/.stsrc/st-alpha-0.7.diff $GITVAR/st/
+
+# configs for terminal applications
+cp -p $MYHOME/.config/mpd/mpd.conf $GITVAR/mpd/
+cp -p $MYHOME/.config/ncmpcpp/config $GITVAR/ncmpcpp/
+cp -p $MYHOME/.config/ncmpcpp/bindings $GITVAR/ncmpcpp/
+cp -p $MYHOME/.config/ranger/commands.py $GITVAR/ranger/
+cp -p $MYHOME/.config/ranger/rc.conf $GITVAR/ranger/
+cp -p $MYHOME/.config/ranger/rifle.conf $GITVAR/ranger/
+cp -p $MYHOME/.config/ranger/scope.sh $GITVAR/ranger/
+cp -p $MYHOME/.config/mps-youtube/transcode $GITVAR/mps-youtube/
+cp -pr $MYHOME/.config/vim/* $GITVAR/vim/
